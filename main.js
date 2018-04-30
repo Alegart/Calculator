2b832fd10787db262ad611d9e796342ff5ecb54a
Dannil Dobrokhvalov <Hyouko1991@gmail.com>
2018-04-30 20:51:25 +0300

    The function of adding decimal numbers is implemented

--- main.js
+++ main.js
@@ -1,28 +1,36 @@
+// Adding signs
+
 function printOper (a) {
 	var elem = document.getElementById('result');
-	if (!!!elem.value) {
+	if (elem.value == '') {
 	 	elem.value;
 	 } else if (elem.value.substr(-1).match(/[\-\×\÷\+]/)) {
 	 	elem.value = elem.value.substr(0, elem.value.length-1) + a;
 	 } else {
 	 	elem.value += a;
 	 }
-};
+}
+
+// Adding numbers and left round brackets
 
 function printNumLBr (a) {
 	document.getElementById('result').value += a;
-};
+}
+
+// Adding right round brackets
 
 function printRBracket (a) {
 	var elem = document.getElementById('result');
-	if ((elem.value.split('').filter(y => y == '(').length == elem.value.split('').filter(y => y == ')').length) || (elem.value.split('').filter(y => y == '(').length == 0)) {
+	if ( (elem.value.split('').filter(y => y == '(').length == elem.value.split('').filter(y => y == ')').length) || (elem.value.split('').filter(y => y == '(').length == 0)) {
 		elem.value;
 	} else if (elem.value.length == 0) {
 		elem.value; 
 	} else {
 		elem.value += a;
 	}
-};
+}
+
+// Value reset
 
 function resetDlt (a) {
 	var elem = document.getElementById('result');
@@ -31,10 +39,29 @@ function resetDlt (a) {
 	} else {
 		elem.value = elem.value.substr(0, elem.value.length-1);
 	}
-};
+}
+
+// Adding decimal numbers
+
+function printPoint (a) {
+	var elem = document.getElementById('result');
+		if (elem.value == '') {
+			elem.value;
+		} else if (elem.value.substr(-1) == '.') {
+			elem.value;
+		} else if (typeof +elem.value.substr(-1) !== 'number') {
+			elem.value;
+		} else if ((elem.value.match(/[\.]/)) && (elem.value.substr(elem.value.lastIndexOf('.'), elem.value.substr(-1)).match(/[\-\×\÷\+]/) == null)) {
+			elem.value;
+		} else {
+			elem.value += a;
+		}
+}
+
+// Calculation
 
 function equal() {
-	var elem = document.getElementById('result'), replace;
-	elem = elem.value.replace('÷', '/').replace('x', '*');
+	var replace = document.getElementById('result').value.replace('÷', '/').replace('x', '*');
 	
-};
+}
+
